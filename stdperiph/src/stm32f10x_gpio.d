506fbc133caf6c1ba24136ff1d1d983f93ba7851
Evgeny Chormonov <en.chormonov@gmail.com>
2018-11-22 02:04:40 +0300

    REFRACT: untrack d files

--- stdperiph/src/stm32f10x_gpio.d
+++
@@ -1,5 +0,0 @@
-stdperiph/src/stm32f10x_gpio.o: stdperiph/src/stm32f10x_gpio.c \
- stdperiph/inc/stm32f10x_gpio.h cmsis/stm32f10x.h cmsis/core_cm3.h \
- f:\programs\codesourcery\sourcery\ g++\ lite\bin\../lib/gcc/arm-none-eabi/4.4.1/../../../../arm-none-eabi/include/stdint.h \
- cmsis/system_stm32f10x.h src/stm32f10x_conf.h \
- stdperiph/inc/stm32f10x_rcc.h
